6fced6b81657b4ed271afc5bcef3908d1b2f7f77
luke01455 <luke0145555@hotmail.co.uk>
2019-11-29 19:46:44 +0000

    had the app send a message to the backend using socket.io

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import io from 'socket.io-client';
 
 export default function HomeScreen() {
 const [messageToSend, setMessageToSend] = useState("");
+const socket = useRef(null);
 
-  useEffect(function() {
-    io("http://192.168.0.10:3001");
+  useEffect(() => {
+    socket.current = io("http://192.168.0.10:3001");
   }, []);
+
+  const sendMessage = () => {
+    socket.current.emit("message", messageToSend);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <TextInput value={messageToSend} 
       onChangeText={(text) => setMessageToSend(text)} 
-      placeholder="Enter chat message.." />
+      placeholder="Enter chat message.."
+      onSubmitEditing={sendMessage}
+      />
     </View>
   );
 }
