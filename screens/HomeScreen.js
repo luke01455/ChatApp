86236f315b456ad02b29e955a2d2029c7190517a
luke01455 <luke0145555@hotmail.co.uk>
2019-11-30 16:44:50 +0000

    added some comments and added code which makes app pad around the keyboard when it appears

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import io from 'socket.io-client';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function HomeScreen() {
+
+// useState to set the state of the message to send
 const [messageToSend, setMessageToSend] = useState("");
+
+// useState to set the state of the message which will be received and shown on screen
 const [recvMessages, setRecvMessages] = useState([]);
+
+// useRef to avoid scoping issues with useEffect
 const socket = useRef(null);
 
+
+// useEffect to listen to backend, send a message to backend and set the messages which have already been sent - which update on reload component
   useEffect(() => {
     socket.current = io("http://192.168.0.10:3001");
     socket.current.on("message", message => {
     setRecvMessages(prevState => [...prevState, message]);
     });
+
+    //using bootstrap suggested message setup
     setRecvMessages([
       {
         _id: 1,
@@ -27,13 +37,15 @@ const socket = useRef(null);
     ]);
   }, []);
 
+  // function which sends the message to send to the backend, then clears out the text view box
   const sendMessage = () => {
     socket.current.emit("message", messageToSend);
     setMessageToSend("");
   };
 
   return (
-
+    <View style={{ flex: 1}}>
+    {/* // Gifted chat bootstrap UI message view */}
       <GiftedChat
         messages={recvMessages}
         // onSend={messages => this.onSend(messages)}
@@ -41,6 +53,11 @@ const socket = useRef(null);
           _id: 1,
         }}
       />
+    {/* Code which makes the app pad around the keyboard when user needs to type something */}
+   {
+      Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
+   }
+    </View>
   );
 }
 
