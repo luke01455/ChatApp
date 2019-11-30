34a9c52631cb550d5742f970c32355d979b46a5b
luke01455 <luke0145555@hotmail.co.uk>
2019-11-30 15:12:18 +0000

    added ability to receive messages and display them from backend

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,19 +4,28 @@ import io from 'socket.io-client';
 
 export default function HomeScreen() {
 const [messageToSend, setMessageToSend] = useState("");
+const [recvMessages, setRecvMessages] = useState([]);
 const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://192.168.0.10:3001");
+    socket.current.on("message", message => {
+    setRecvMessages(prevState => [...prevState, message]);
+    })
   }, []);
 
   const sendMessage = () => {
     socket.current.emit("message", messageToSend);
-  }
+    setMessageToSend("");
+  };
+
+  const textOfRecvMessages = recvMessages.map(msg => (
+    <Text key={msg}>{msg}</Text>
+  ));
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {textOfRecvMessages}
       <TextInput value={messageToSend} 
       onChangeText={(text) => setMessageToSend(text)} 
       placeholder="Enter chat message.."
